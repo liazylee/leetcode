fd3446c120a667d1314d4cfe2882d4385dbb525d
zhenyili <lizhenyi1314>
2023-12-25 21:39:25 +0000

    25 Dec

---
+++ editor/en/[19]Remove Nth Node From End of List.py
@@ -0,0 +1,67 @@
+"""
+<p>Given the <code>head</code> of a linked list, remove the <code>n<sup>th</sup></code> node from the end of the list and return its head.</p>
+
+<p>&nbsp;</p> 
+<p><strong class="example">Example 1:</strong></p> 
+<img alt="" src="https://assets.leetcode.com/uploads/2020/10/03/remove_ex1.jpg" style="width: 542px; height: 222px;" /> 
+<pre>
+<strong>Input:</strong> head = [1,2,3,4,5], n = 2
+<strong>Output:</strong> [1,2,3,5]
+</pre>
+
+<p><strong class="example">Example 2:</strong></p>
+
+<pre>
+<strong>Input:</strong> head = [1], n = 1
+<strong>Output:</strong> []
+</pre>
+
+<p><strong class="example">Example 3:</strong></p>
+
+<pre>
+<strong>Input:</strong> head = [1,2], n = 1
+<strong>Output:</strong> [1]
+</pre>
+
+<p>&nbsp;</p> 
+<p><strong>Constraints:</strong></p>
+
+<ul> 
+ <li>The number of nodes in the list is <code>sz</code>.</li> 
+ <li><code>1 &lt;= sz &lt;= 30</code></li> 
+ <li><code>0 &lt;= Node.val &lt;= 100</code></li> 
+ <li><code>1 &lt;= n &lt;= sz</code></li> 
+</ul>
+
+<p>&nbsp;</p> 
+<p><strong>Follow up:</strong> Could you do this in one pass?</p>
+
+<div><div>Related Topics</div><div><li>Linked List</li><li>Two Pointers</li></div></div><br><div><li>👍 17688</li><li>👎 738</li></div>
+"""
+from typing import Optional
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if not head:
+            return head
+        dummy = ListNode()
+        dummy.next = head
+        slow = fast = dummy
+        for _ in range(n):
+            fast = fast.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return dummy.next
+
+# leetcode submit region end(Prohibit modification and deletion)
